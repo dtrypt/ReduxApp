269e75c92e2686f78b12fa9b25161c2ffdfcdb95
trustinruss <russkranz@yahoo.com>
2017-05-14 17:55:40 -0700

    change state with action creators, click on a book, book-detail changes

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,8 @@
 export function selectBook(book) {
   //selectBook is an ActionCreator, needs to return an action,
-  // an object with a property.
+  // an object with a type property & payload.
   return {
-
+    type: 'BOOK_SELECTED',
+    payload: book
   };
 }
